2e549ae1891bd7d3d59da7f53dfb12f7f64ed0c5
tassio <tassiokp206@gmail.com>
2021-08-02 10:58:34 -0300

    update script sql server

--- 1 - SQL Server/Script.sql
+++ 1 - SQL Server/Script.sql
@@ -70,4 +70,53 @@ update Clientes set Codigo = 6 AND Nome = 'Adidas' where TipoPessoa = 'J';
 
 delete from clientes where Nome = 'Adidas';
 
+insert Produtos values
+(1, 'Caneca', 'Caneca Azul', 1.5),
+(2, 'Caderno', 'Caderno 10 Matérias', 1.5)
+
+select * from Produtos;
+
+insert Pedido values
+(1, getDate(), 0, 3, 3);
+
+insert PedidoItem values
+(1, 1, 1.5, 2)
+
+insert Pedido values
+(2, getDate(), 0, 22.49, 1);
+
+insert PedidoItem values
+(2, 1, 1.5, 1),
+(2, 2, 20.99, 1);
+
+select * from Pedido;
+
+select * from PedidoItem;
+
+select isnull(TipoPessoa, getDate())
+from clientes;
+
+select *,
+	case 
+		when TipoPessoa = 'J' then 'Juridica'
+		when TipoPessoa = 'F' then 'Fisica'
+		else 'Pessoa Indefinida'
+	end + convert(varchar, GETDATE() , 103) as PessoaCustom
+from clientes;
+
+select *,
+convert(varchar, DataSolicitacao, 103) as DataBR
+from Pedido;
+
+select *
+from clientes;
+
+alter table Clientes add constraint pk_cliente primary key (Codigo)
+
+alter table Clientes drop constraint pk_cliente;
+
+select * from pedido;
+
+
+
 -- drop table Teste;
